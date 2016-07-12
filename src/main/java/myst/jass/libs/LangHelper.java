db3512e08f339517a9456666bf99221bc7eb0dd7
manmaed <manmaed18@gmail.com>
2016-07-12 20:20:40 +0100

    FILES and stuff

---
+++ src/main/java/myst/jass/libs/LangHelper.java
@@ -0,0 +1,22 @@
+package manmaed.stargatemod.libs;
+
+import net.minecraft.util.text.translation.I18n;
+
+public enum LangHelper {
+    TAB("tab"),
+    NONE("");
+
+    private String name;
+
+    LangHelper(String name)
+    {
+        this.name = name;
+    }
+
+    public String translateMessage(String message) {
+        if (this.name == "")
+            return I18n.translateToLocal(message);
+
+        return I18n.translateToLocal(String.format("%s.%s.%s", this.name, Reference.MOD_ID, message + ""));
+    }
+}
---
+++ src/main/java/myst/jass/libs/Reference.java
@@ -0,0 +1,16 @@
+package manmaed.stargatemod.libs;
+
+/**
+ * Created by manmaed on 22/02/2016.
+ */
+public class Reference {
+    public static final String MOD_ID = "stargatemod";
+    public static final String RE_PREFIX = MOD_ID.toLowerCase() + ":";
+    public static final String MOD_NAME = "StarGate Mod";
+    public static final String VERSION = "0.0.0-dev";
+    public static final String FONT_LOCATION = "/fonts/";
+    public static final String CLIENT_PROXY = "manmaed.stargatemod.proxy.ClientProxy";
+    public static final String PROXY_COMMON = "manmaed.stargatemod.proxy.CommonProxy";
+
+
+}
